cd5db504e16b6c06d5240f16d863f2e4742bf0f8
Andrew Wells <andrew@vin.li>
2015-07-30 09:05:30 -0500

    Initial Commit

---
+++ VLTest/VLTest.h
@@ -0,0 +1,19 @@
+//
+//  VLTest.h
+//  VLTest
+//
+//  Created by Andrew Wells on 7/30/15.
+//  Copyright (c) 2015 Andrew Wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VLTest.
+FOUNDATION_EXPORT double VLTestVersionNumber;
+
+//! Project version string for VLTest.
+FOUNDATION_EXPORT const unsigned char VLTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VLTest/PublicHeader.h>
+
+
